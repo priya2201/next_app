d02386856f567ba9f2306fbb2dcc7fca6633f587
priya2201 <priyamurpani99@gmail.com>
2024-10-09 10:31:43 +0530

    apis

--- training_backend/userController.ts
+++ training_backend/userController.ts
@@ -22,11 +22,15 @@ export const userSignup = async (
       res.status(400).json({ errors: validationError });
       return;
     }
-    const { email, phone, password,confirmPassword } = UserInputs;
+    const { email, phone, password, confirmPassword } = UserInputs;
     console.log(UserInputs, "ui");
+    if (password !== confirmPassword) {
+      res.status(400).json({ message: "Passwords do not match" });
+      return;
+    }
+
     const salt = await bcrypt.genSalt();
     const hashedPassword = await bcrypt.hash(password, salt);
-
     const existingUser = await User.findOne({ email });
     if (existingUser) {
       res.status(400).json({ message: "Email already exists" });
@@ -40,7 +44,7 @@ export const userSignup = async (
       address: req.body.address,
       firstName: req.body.firstName,
       lastName: req.body.lastName,
-      confirmPassword
+      confirmPassword,
     });
     res.status(201).json({ message: "User Created Successfully", result });
   } catch (error) {
@@ -57,49 +61,47 @@ export const userLogin = async (
   res: Response,
   next: NextFunction
 ): Promise<void> => {
-  try{
-  const UserInputs = plainToClass(LoginInput, req.body);
-  console.log(req.body);
-  const validationError = await validate(UserInputs, {
-    validationError: { target: true },
-  });
+  try {
+    const UserInputs = plainToClass(LoginInput, req.body);
+    console.log(req.body);
+    const validationError = await validate(UserInputs, {
+      validationError: { target: true },
+    });
 
-  if (validationError.length > 0) {
-    res.status(400).json({errors:validationError});
-    return;
-  }
-  const { email, password,rememberMe } = UserInputs;
+    if (validationError.length > 0) {
+      res.status(400).json({ errors: validationError });
+      return;
+    }
+    const { email, password, rememberMe } = UserInputs;
     const isUser = await User.findOne({ email: email });
     if (!isUser) {
       res.status(404).json({ message: "User not found" });
       return;
     }
 
-
     const validation = await bcrypt.compare(password, isUser.password);
     if (!validation) {
       res.status(401).json({ message: "Invalid credentials" });
       return;
     }
-      const signature = await jwt.sign(
-        { _id: isUser._id, email: isUser.email },
-        "APP_SECRET",
-        { expiresIn: rememberMe ? '3d':'1d' }
-      );
-      res.status(200).json({
-        signature,
-        email: isUser.email,
-      });
-    }catch (error) {
-      if (error instanceof Error) {
-        res.status(500).json({ message: error.message });
-      } else {
-        res.status(500).json({ message: "An unknown error occurred" });
-      }
-  
+    const signature = await jwt.sign(
+      { _id: isUser._id, email: isUser.email },
+      "APP_SECRET",
+      { expiresIn: rememberMe ? "3d" : "1d" }
+    );
+    res.status(200).json({
+      signature,
+      email: isUser.email,
+    });
+    console.log(signature, "signature");
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(500).json({ message: error.message });
+    } else {
+      res.status(500).json({ message: "An unknown error occurred" });
+    }
   }
-}
- 
+};
 
 export const userLogout = async (
   req: Request,
@@ -107,27 +109,24 @@ export const userLogout = async (
   next: NextFunction
 ): Promise<void> => {
   try {
-    // Clear the JWT token stored in the cookie
     res.clearCookie("token", {
       httpOnly: true,
       secure: true,
       sameSite: "strict",
     });
 
-    // Optionally, return a success message
     res.status(200).json({ message: "Logged out successfully" });
     return;
+    
   } catch (error) {
     {
       if (error instanceof Error) {
         res
           .status(500)
           .json({ message: "Logout failed", error: error.message });
-      }
-      else {
+      } else {
         res.status(500).json({ message: "An unknown error occurred" });
       }
-  
     }
   }
 };
--- training_backend/userValidator.dto.ts
+++ training_backend/userValidator.dto.ts
@@ -1,27 +1,32 @@
-import { IsEmail, Length ,IsBoolean} from "class-validator";
+import { IsEmail, Length, IsBoolean, IsNotEmpty } from "class-validator";
 
 export class UserInput {
   @IsEmail()
+  @IsNotEmpty()
   email!: string;
 
   @Length(10)
+  @IsNotEmpty()
   phone!: string;
 
   @Length(6, 12)
+  @IsNotEmpty()
   password!: string;
 
   @Length(6, 12)
+  @IsNotEmpty()
   confirmPassword!: string;
 }
 
 export class LoginInput {
   @IsEmail()
+  @IsNotEmpty()
   email!: string;
 
   @Length(6, 12)
+  @IsNotEmpty()
   password!: string;
 
   @IsBoolean()
   rememberMe!: boolean;
-
 }
